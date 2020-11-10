79244840c8b8901cdce6ac0f872030ed1118c39b
Alan Baldwin <baldwingaming1979@gmail.com>
2020-11-10 20:09:06 +0000

    added font-awesome packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
