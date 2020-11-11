997b1928194f46a9551b17a7372f541d33c36083
Alan Baldwin <baldwingaming1979@gmail.com>
2020-11-11 08:28:57 +0000

    input slider

--- src/components/Player.js
+++ src/components/Player.js
@@ -32,6 +32,10 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
       Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
     );
   };
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
   //state
   const [songInfo, setSongInfo] = useState({
     currentTime: null,
@@ -42,7 +46,13 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input type="range" />
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+          type="range"
+        />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
