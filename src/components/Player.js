8f6ec39ef432d82541b89d52bc02e44cbbd813e8
Alan Baldwin <baldwingaming1979@gmail.com>
2020-11-14 18:33:15 +0000

    customising input

--- src/components/Player.js
+++ src/components/Player.js
@@ -77,19 +77,31 @@ const Player = (
     playAudio(isPlaying, audioRef);
     //layAudio(audioRef, isPlaying).then();
   };
-  //state
+  const trackAnim = {
+    transform: `translateX(${songInfo.animationPercentage}%)`,
+  };
 
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input
-          min={0}
-          max={songInfo.duration || 0}
-          value={songInfo.currentTime}
-          onChange={dragHandler}
-          type="range"
-        />
+        <div
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]}, ${
+              currentSong.color[1]
+            })`,
+          }}
+          className="track"
+        >
+          <input
+            min={0}
+            max={songInfo.duration || 0}
+            value={songInfo.currentTime}
+            onChange={dragHandler}
+            type="range"
+          />
+          <div style={trackAnim} className="animate-track"></div>
+        </div>
         <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
       </div>
 
